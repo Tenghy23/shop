9a7574774ece648082284b1cabc3c909cf80a2b3
Tenghy <tenghuiyong@outlook.sg>
2023-12-31 14:39:32 +0800

    split program.cs & startup.cs

---
+++ Application/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Application.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+                });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+
+            return app;
+        }
+    }
+}
+
+/* An Extension class in C# could simply be a child class( inheriting and adding more
+ * functionality to the base type or interface) or a static class */
--- Application/Program.cs
+++ Application/Program.cs
@@ -1,25 +1,64 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Application
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            // any code inside using will be disposed after all the methods ran 
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                //try
+                //{
+                //    var context = services.GetRequiredService<StoreDbContext>();
+                //    await context.Database.MigrateAsync();
+                //    await StoreContextSeed.SeedAsync(context, loggerFactory);
+                //}
+                //catch (Exception ex)
+                //{
+                //    var logger = loggerFactory.CreateLogger<Program>();
+                //    logger.LogError(ex, "An error occured during migration");
+                //}
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//var builder = WebApplication.CreateBuilder(args);
 
-app.MapControllers();
+//// Add services to the container.
 
-app.Run();
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
