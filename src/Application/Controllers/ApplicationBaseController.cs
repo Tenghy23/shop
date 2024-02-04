8423bc9a1dfa6194c9b21ddbc6119fe7132714d4
Tenghy <tenghuiyong@outlook.sg>
2024-02-04 23:17:32 +0800

    update exception handling + add sample controller

---
+++ src/Application/Controllers/ApplicationBaseController.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.Controllers
+{
+    public class ApplicationBaseController : Controller
+    {
+        protected readonly ILogger _logger;
+
+        private string LogFormat
+        {
+            get => "Message: { Message }, Application.Class: " + GetType().Name + ".Method: { Method }";
+        }
+
+        protected IActionResult InvalidResponse(Exception ex, [CallerMemberName] string caller = "")
+        {
+            return ExceptionHandlingHelper.ReturnNonSuccessResponse(LogFormat, _logger, ex, caller);
+        }
+    }
+}
---
+++ src/Application/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿namespace Application.Controllers
+{
+    [ApiController]
+    [Route("[product]")]
+    public class ProductController : ApplicationBaseController
+    {
+        [HttpPost("getAllProducts")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            try
+            {
+                var response = true; // to update
+                return Ok(response);
+            }
+            catch (Exception ex) 
+            {
+                return InvalidResponse(ex);
+            }
+        }
+    }
+}
