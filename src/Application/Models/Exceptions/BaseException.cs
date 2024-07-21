0d81cd728b8d193a6338528a887e51227960f9f1
Tenghy <tenghuiyong@outlook.sg>
2024-07-22 00:34:38 +0800

    global exception handler

---
+++ src/Application/Models/Exceptions/BaseException.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Models.Exceptions
+{
+    public class BaseException : Exception
+    {
+        public HttpStatusCode StatusCode { get; }
+        public BaseException(string message, HttpStatusCode statusCode = HttpStatusCode.InternalServerError)
+            : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
---
+++ src/Application/Models/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Models.Exceptions
+{
+    public class ProductNotFoundException : BaseException
+    {
+        public ProductNotFoundException(Guid id)
+            : base($"product with id {id} not found", HttpStatusCode.NotFound)
+                {
+                }
+    }
+}
