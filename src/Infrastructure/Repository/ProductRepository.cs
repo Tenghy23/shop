ec771d32776647a3ea21b46d8c68a25ac364b48e
Damien <damien_teng@outlook.com>
2024-11-21 13:16:58 +0800

    caching sample

--- src/Infrastructure/Repository/ProductRepository.cs
+++ src/Infrastructure/Repository/ProductRepository.cs
@@ -1,13 +1,16 @@
-﻿using System.Linq.Expressions;
+﻿using Microsoft.Extensions.Caching.Memory;
+using System.Linq.Expressions;
 
 namespace Infrastructure.Data
 {
     public class ProductRepository : IProductRepository
     {
         private readonly StoreDbContext _dbContext;
-        public ProductRepository(StoreDbContext context)
+        private readonly IMemoryCache _memoryCache;
+        public ProductRepository(StoreDbContext context, IMemoryCache memoryCache)
         {
             _dbContext = context;
+            _memoryCache = memoryCache;
         }
 
         public async Task<IEnumerable<Product>> RetrieveProductAsync(Expression<Func<Product, bool>> criteria)
@@ -18,6 +21,34 @@ namespace Infrastructure.Data
                 .Where(criteria);
 
             return await query.ToListAsync() ?? Enumerable.Empty<Product>();
+        }
+
+        public async Task<IEnumerable<Product>> RetrieveProductAsync(Guid id)
+        {
+            // Generate a unique cache key based on the method and parameters
+            string cacheKey = $"Product_{id}";
+
+            // Try to retrieve the result from the cache
+            if (_memoryCache.TryGetValue(cacheKey, out IEnumerable<Product> cachedProducts))
+            {
+                return cachedProducts;
+            }
+
+            // If not cached, query the database
+            var query = _dbContext.Products
+                .AsNoTracking()
+                .AsSplitQuery()
+                .Where(x => x.Id == id);
+
+            var products = await query.ToListAsync() ?? Enumerable.Empty<Product>();
+
+            // Cache the result with a sliding expiration of 10 minutes
+            _memoryCache.Set(cacheKey, products, new MemoryCacheEntryOptions
+            {
+                SlidingExpiration = TimeSpan.FromMinutes(10)
+            });
+
+            return products;
         }
 
         public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
